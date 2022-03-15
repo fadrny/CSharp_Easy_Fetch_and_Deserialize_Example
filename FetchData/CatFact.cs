0c760b21afcb2a913d5a560c030a2abb625a6869
marekParek <48125221+fadrny@users.noreply.github.com>
2022-03-15 18:48:25 +0100

    Add project files.

---
+++ FetchData/CatFact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FetchData
+{
+    public class CatFact
+    {
+        //public string length;
+
+        public string fact { get; set; }
+        public int length { get; set; }
+    }
+}
---
+++ FetchData/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+
+namespace FetchData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+
+            // Add a user agent header in case the
+            // requested URI contains a query.
+
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+            Stream data = client.OpenRead("https://catfact.ninja/fact");
+            StreamReader reader = new StreamReader(data);
+            string s = reader.ReadToEnd();
+            data.Close();
+            reader.Close();
+
+            Console.WriteLine("JSON: " + s);
+
+            var objectWithFields = System.Text.Json.JsonSerializer.Deserialize<CatFact>(s);
+            Console.WriteLine("\r\nDélka je: " + objectWithFields.length);
+            Console.ReadKey();
+        }
+    }
+}
